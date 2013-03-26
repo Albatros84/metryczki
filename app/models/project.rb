612636938dabebff163ee43eb11573bc5fa76ab8
Janusz Liro <j.liro@pracowniagier.com>
2013-03-26 17:12:23 +0100

    added author accessors

--- app/models/project.rb
+++ app/models/project.rb
@@ -21,7 +21,15 @@ class Project < ActiveRecord::Base
   :course_of_training, :on_fly_findings_and_suggestions, :project_evaluation, :proposals_for_sales_potential,
   :merytoryka, :conduct_and_summary_of_game, :notes_on_materials, :notes_on_organization, :game_ids, :numer_ks, :user_roles_id
   
-  attr_accessible :responsibilities_division_author
+  attr_accessible :responsibilities_division_author, :client_author, :highrise_author, :name_author, :project_type_author, :skydrive_author, :basecamp_author,
+  :participant_count_author, :game_author, :groups_division_author, :number_of_rooms_author, :date_of_game_author, :hour_duration_author,
+  :localization_author, :contacts_client_author, :contacts_hotel_author, :transport_details_author, :accomodation_author,
+  :target_agreed_with_customer_author, :other_expectations_author, :subject_author, :profile_of_participants_author,
+  :contact_person_on_client_side_author, :host_of_the_meeting_author, :other_information_author, :leading_head_coach_author,
+  :second_coach_author, :cpd_trainee_author, :assistants_author, :agenda_of_meeting_author, :responsibilities_division_author,
+  :after_game_summary_author, :invitation_for_participants_author, :purpose_and_other_expectations_author,:participants_short_description_in_groups_author,
+  :course_of_training_author, :on_fly_findings_and_suggestions_author, :project_evaluation_author, :proposals_for_sales_potential_author,
+  :merytoryka_author, :conduct_and_summary_of_game_author, :notes_on_materials_author, :notes_on_organization_author, :game_ids_author, :numer_ks_author, :user_roles_id_author
     
   validates_presence_of :name, :on => :create
   validates_presence_of :client, :on => :create
